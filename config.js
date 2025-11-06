929788d0981d3e85826a793a582a6d6903c8b62a
Einartig <vera.eulenstein@gmx.net>
2025-11-06 14:46:12 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcqNwRZFZTDwemmIfY1CDfAchxoxzqZao",
+  authDomain: "finfluencer-study.firebaseapp.com",
+  projectId: "finfluencer-study",
+  storageBucket: "finfluencer-study.firebasestorage.app",
+  messagingSenderId: "351256999007",
+  appId: "1:351256999007:web:0f7aa58d3704af700197ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
